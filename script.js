b824734ba2cda88a859081f0867ed718e16f3d28
Christian Trublet de Nermont <cjwtdn@gmail.com>
2022-06-15 15:12:24 -0600

    FINALLY got the quote generator to work correctly. added new quotes and edited hyperlinks to show as clips.

--- script.js
+++ script.js
@@ -1,24 +1,34 @@
 var quotes = [
-    '"If you can dodge a wrench, you can dodge a ball." - Patches O\'Houlihan',
-    `"When I was a kid, when I was a little boy, I always wanted to be a dinosaur. I wanted to be a Tyrannosaurus Rex more than anything in the world. I made my arms short and I roamed the backyard, I chased the neighborhood cats, I growled and I roared. Everybody knew me and was afraid of me. And one day my dad said, "Bobby, you are 17. It's time to throw childish things aside," and I said, "Okay, Pop." But he didn't really say that, he said, "Stop being a f***ing dinosaur and get a job."
-    - Dr. Robert Doback`,
-    '"Ma! The meatloaf!!" - Chazz Reinhold',
-    '"You miss 100% of the shots you don\'t take. - Wayne Gretzky." - Michael Scott',
-    '"Identity theft is a real crime!" - Dwight Schrute',
-    '"It\'s the history of the Totnum" - Giorgio Chiellini',
+    '"If you can dodge a wrench, you can dodge a ball." - Patches O\'Houlihan, <em>Dodgeball</em>',
+    `"When I was a kid, when I was a little boy, I always wanted to be a dinosaur. I wanted to be a Tyrannosaurus Rex more than anything in the world. I made my arms short and I roamed the backyard, I chased the neighborhood cats, I growled and I roared. Everybody knew me and was afraid of me. And one day my dad said, 'Bobby, you are 17. It\'s time to throw childish things aside,' and I said, 'Okay, Pop.' But he didn't really say that, he said, 'Stop being a f***ing dinosaur and get a job.'"
+    - Dr. Robert Doback, <em>Step Brothers</em>`,
+    '"Ma! The meatloaf!!" - Chazz Reinhold, <em>Wedding Crashers</em>',
+    '"You miss 100% of the shots you don\'t take. - Wayne Gretzky." - Michael Scott, <em>The Office</em>',
+    '"Identity theft is a real crime!" - Dwight Schrute, <em>The Office</em>',
+    '"It\'s the history of the Totnum" - Giorgio Chiellini, <em>Juventus vs. Tottenham Hotspur</em>',
     '"My money don\'t jiggle jiggle, it folds" - Louis Theroux',
-    '"It\'s <a href = url "https://www.facebook.com/Arsenal/videos/reyes-rocket-against-middlesbrough/10155287626067713/"> REYEEEESS!!!</a>." - Martin Tyler',
+    '"It\'s <a href="https://youtube.com/clip/Ugkxwj06_bq6oPEcEKDcsaZzE-9YRNLGy8sP"> REYEEEESS!!!</a>" - Martin Tyler, <em>Arsenal vs Middlesbrough, 2004 </em>',
     '"That\'s one of the great things about music. You can sing a song to 85,000 people and they\'ll sing it back for 85,000 different reasons." - Dave Grohl',
-    '"Trick question. Lemmy IS God". - Rex',
-    '"I play real sports, not trying to be the best at exercising." - Kenny Powers',
-    '“Oh wait, next weeks no good for me, the Jonas brothers are in town.” - Alan',
-    '"I like to think of Jesus like, with giant eagles\' wings and singin\' lead vocals for Lynyrd Skynyrd with like an Angel Band, and I\'m in the front row, and I\'m hammered drunk." - Cal Naughton Jr.',
-    '"We built DIFFERENT." - Lebron James',
+    '"Trick question. Lemmy IS God". - Rex, <em>Airheads</em>',
+    '"I play real sports, not trying to be the best at exercising." - Kenny Powers, <em>Eastbound and Down</em>',
+    '“Oh wait, next weeks no good for me, the Jonas brothers are in town.” - Alan, <em>The Hangover</em>',
+    '"I like to think of Jesus like, with giant eagles\' wings and singin\' lead vocals for Lynyrd Skynyrd with like an Angel Band, and I\'m in the front row, and I\'m hammered drunk." - Cal Naughton Jr., <em>Talladega Nights</em>',
+    '"We built DIFFERENT." - Lebron James, <em> 2020 Playoff bubble</em>',
     '“Resentment is like drinking poison and then hoping it will kill your enemies.” - Nelson Mandela',
-    '"We\'ll do it live!" - Bill o\'Reilly',
+    '"We\'ll do it live!" - Bill o\'Reilly, <em>Entertainment Tonight</em>',
+    '"Kim, there\'s people that are <a href="https://youtube.com/clip/UgkxbD8cRHWPuIa4GEZ9pCG3h5RpxCwdSOlJ"> dying</a>." - Kourtney Kardashian, <em>KUWTK</em>',
 ]
-
-function newQuote() {
-    var randomNumber = Math.floor(Math.random() * (quotes.length));
-    document.getElementById('showQuote').innerHTML = quotes[randomNumber];
-}
+var quoteButton = document.getElementById('quotBtn');
+var generate = document.getElementById('quoteDisplay');
+    
+quoteButton.addEventListener('click', () => {
+    if(quotes.length) {
+        
+        let random = Math.floor(Math.random() * quotes.length);
+        generate.innerHTML = quotes[random];
+        quotes.splice(random, 1);
+        
+    } else {
+        generate.innerHTML = "No more quotes!";
+    }
+})
